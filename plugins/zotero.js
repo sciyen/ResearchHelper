5f2d415fae341a46d1f427166f97a71a4b87699f
sciyen <a885566885566@gmail.com>
2022-02-26 16:16:00 +0800

    UI enhancement
    Build the citations with tree

--- plugins/zotero.js
+++ plugins/zotero.js
@@ -143,10 +143,118 @@ function setupZoretoMenu(ui) {
 	};
 }
 
+var TagSelectorWindow = function(editorUi, x, y, w, h){
+	var graph = editorUi.editor.graph;
+
+	var div = document.createElement('div');
+	div.style.overflow = 'hidden';
+	div.style.padding = '12px 8px 12px 8px';
+	div.style.height = 'auto';
+
+	var filterInput = document.createElement('input');
+	filterInput.setAttribute('placeholder', 'Type in the tags and press Enter to add them');
+	filterInput.setAttribute('type', 'text');
+	filterInput.style.width = '100%';
+	filterInput.style.boxSizing = 'border-box';
+	filterInput.style.fontSize = '12px';
+	filterInput.style.borderRadius = '4px';
+	filterInput.style.padding = '4px';
+	filterInput.style.marginBottom = '8px';
+	filterInput.setAttribute('placeholder', 'Filter tags');
+	div.appendChild(filterInput);
+
+	this.window = new mxWindow(mxResources.get('tagSelector'), div, x, y, w, null, true, true);
+	this.window.destroyOnClose = false;
+	this.window.setMaximizable(false);
+	this.window.setResizable(true);
+	this.window.setScrollable(true);
+	this.window.setClosable(true);
+	this.window.contentWrapper.style.overflowY = 'scroll';
+
+	mxEvent.addListener(filterInput, 'keyup', function(){
+		// Do something
+		console.log('keyup')
+	});
+
+	this.window.addListener('show', mxUtils.bind(this, function(){
+		this.window.fit();
+		
+		if (this.window.isVisible()){
+		}
+		else{
+			graph.container.focus();
+		}
+	}));
+	
+	this.window.setLocation = function(x, y){
+		var iw = window.innerWidth || document.body.clientWidth || document.documentElement.clientWidth;
+		var ih = window.innerHeight || document.body.clientHeight || document.documentElement.clientHeight;
+		
+		x = Math.max(0, Math.min(x, iw - this.table.clientWidth));
+		y = Math.max(0, Math.min(y, ih - this.table.clientHeight - 48));
+
+		if (this.getX() != x || this.getY() != y){
+			mxWindow.prototype.setLocation.apply(this, arguments);
+		}
+	};
+	
+	var resizeListener = mxUtils.bind(this, function(){
+		var x = this.window.getX();
+		var y = this.window.getY();
+		
+		this.window.setLocation(x, y);
+	});
+	
+	mxEvent.addListener(window, 'resize', resizeListener);
+
+	this.destroy = function(){
+		mxEvent.removeListener(window, 'resize', resizeListener);
+		this.window.destroy();
+	}
+}
+
+function setupTagSelector(ui){
+	// Adds resource for action
+    mxResources.parse('tagSelector=Tag Selector');
+
+    // Adds action
+    ui.actions.addAction('tagSelector...', () => {
+        if (editorUi.tagSelectorWindow == null)
+		{
+			editorUi.tagSelectorWindow = new TagSelectorWindow(editorUi, document.body.offsetWidth - 380, 120, 300, 240);
+			editorUi.tagSelectorWindow.window.addListener('show', function()
+			{
+				editorUi.fireEvent(new mxEventObject('tagSelector'));
+			});
+			editorUi.tagSelectorWindow.window.addListener('hide', function()
+			{
+				editorUi.fireEvent(new mxEventObject('tagSelector'));
+			});
+			editorUi.tagSelectorWindow.window.setVisible(true);
+			editorUi.fireEvent(new mxEventObject('tagSelector'));
+		}
+		else
+		{
+			editorUi.tagSelectorWindow.window.setVisible(!editorUi.tagSelectorWindow.window.isVisible());
+		}
+    });
+
+    // Adds menu item for refreshing
+    let menu = ui.menus.get('extras');
+	let oldFunct = menu.funct;
+	
+	menu.funct = function(menu, parent)
+	{   
+        oldFunct.apply(this, arguments);
+        ui.menus.addMenuItems(menu, ['-', 'Tag Selector'], parent);
+	};
+}
+
 script.onload = () => {
 zoteroApi = ZoteroApiClient.default;
 Draw.loadPlugin(function (ui) {
     setupZoretoMenu(ui);
+	setupTagSelector(ui);
 
 	// Adds numbered toggle property
 	Editor.commonVertexProperties.push({
